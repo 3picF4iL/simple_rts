943da340bdafa08f87f814fd64a32805debe68e9
3picF4iL <haskypl1@gmail.com>
2025-04-01 19:36:39 +0200

    core/building_manager: add manager for buildings
    Manage buildings, production queue, unit production
    firing to enemies etc.
    
    Signed-off-by: 3picF4iL <haskypl1@gmail.com>

---
+++ core/building_manager.py
@@ -0,0 +1,26 @@
+import arcade
+from core.building import Building
+
+class BuildingManager:
+    def __init__(self):
+        self.buildings = arcade.SpriteList(use_spatial_hash=True)
+
+    def add_building(self, x, y):
+        building = Building(x, y)
+        self.buildings.append(building)
+
+    def get_buildings(self):
+        return self.buildings
+
+    def draw(self):
+        self.buildings.draw()
+        for building in self.buildings:
+            if building.selected:
+                arcade.draw_lrbt_rectangle_outline(
+                    building.center_x - building.width / 2,
+                    building.center_x + building.width / 2,
+                    building.center_y - building.height / 2,
+                    building.center_y + building.height / 2,
+                    arcade.color.YELLOW,
+                    2
+                )
