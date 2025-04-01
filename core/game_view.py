8c41ce3ecbdb2a25457960bfb4fc2243a12103aa
3picF4iL <haskypl1@gmail.com>
2025-04-01 19:38:52 +0200

    core/game_view: initial game view
    Simple object presenting, units, buildings
    Collision check for units -> buildings
    Units selecting and moving to the point
    (for now, without pathfinding)
    
    Selecting with graphical presentation (yellow
    outline)
    
    Signed-off-by: 3picF4iL <haskypl1@gmail.com>

---
+++ core/game_view.py
@@ -0,0 +1,70 @@
+import arcade
+from core.building_manager import BuildingManager
+from core.unit_manager import UnitManager
+from core.selection_manager import SelectionManager
+
+
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.buildings = None
+        self.unit_manager = None
+        self.selection_manager = None
+        self.setup()
+
+    def setup(self):
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        self.unit_manager = UnitManager()
+        self.selection_manager = SelectionManager()
+        self.building_manager = BuildingManager()
+
+    def on_show_view(self):
+        print("On show")
+        self.building_manager.add_building(300, 300)
+        self.building_manager.add_building(600, 400)
+        self.unit_manager.spawn_unit(100, 300)
+        self.unit_manager.spawn_unit(400, 100)
+
+    def on_draw(self):
+        self.clear()
+        self.unit_manager.draw()
+        self.building_manager.draw()
+        self.selection_manager.draw()
+
+    def on_update(self, delta_time):
+        self.unit_manager.update(delta_time, self.building_manager.buildings)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.selection_manager.start_selection(x, y)
+        elif button == arcade.MOUSE_BUTTON_RIGHT:
+            self.unit_manager.command_move(
+                self.selection_manager.selected_objects,
+                x, y
+            )
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            dx = abs(self.selection_manager.start_x - x)
+            dy = abs(self.selection_manager.start_y - y)
+
+            if dx < 5 and dy < 5:
+                self.selection_manager.select_by_click(
+                    self.unit_manager.get_units(),
+                    self.building_manager.get_buildings(),
+                    x, y
+                )
+            else:
+                self.selection_manager.select_by_box(
+                    self.unit_manager.get_units(),
+                    self.selection_manager.start_x,
+                    self.selection_manager.start_y,
+                    x, y
+                )
+
+        self.selection_manager.selecting = False
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        if self.selection_manager.selecting:
+            self.selection_manager.update_selection(x, y)
