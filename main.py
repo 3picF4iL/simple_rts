d43d958ecc1dcff5dd23a7d67fb65d07d052851e
3picF4iL <haskypl1@gmail.com>
2025-04-03 00:50:48 +0200

    main: changed resolution and window location
    Signed-off-by: 3picF4iL <haskypl1@gmail.com>

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from utils.i18n import init_translator
 def main():
     init_translator("pl")
 
-    window = arcade.Window(1280, 720, "RTS Etap 1")
+    window = arcade.Window(1440, 900, "RTS")
     view = GameView()
     window.show_view(view)
+    window.set_location(100, 50)
     arcade.run()
 
 if __name__ == "__main__":
