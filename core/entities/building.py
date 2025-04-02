138b77a18abcbc22fc2114c45ab40b273dafa08d
3picF4iL <haskypl1@gmail.com>
2025-04-02 10:11:14 +0200

    core/entities/building: split buildings to sep entities
    Signed-off-by: 3picF4iL <haskypl1@gmail.com>

--- core/entities/building.py
+++ core/entities/building.py
@@ -1,11 +1,49 @@
 import arcade
+from utils.i18n import t
 
 class Building(arcade.Sprite):
-    def __init__(self, x, y):
-        texture = arcade.make_soft_square_texture(60, arcade.color.RED, 255)
-        super().__init__(texture, center_x=x, center_y=y)
-
+    def __init__(self, x, y, width=60, height=60):
+        super().__init__(center_x=x, center_y=y, width=width, height=height)
         self.selected = False
 
-    def setup(self):
-        pass
+    def get_hover_info(self) -> str:
+        return "Building"
+
+    def get_actions(self) -> list:
+        return []
+
+# ------------------------------
+# Building types
+# ------------------------------
+
+class MilitaryBuilding(Building):
+    def get_hover_info(self) -> str:
+        return "Military Building"
+
+
+class EconomicBuilding(Building):
+    def get_hover_info(self) -> str:
+        return "Economic Building"
+
+
+# ------------------------------
+# Buildings list
+# ------------------------------
+
+class Barracks(MilitaryBuilding):
+    def __init__(self, x, y):
+        width = 100
+        height = 100
+        self.name = "barracks"
+
+        texture = arcade.make_soft_square_texture(100, arcade.color.RED, 255)
+        super().__init__(x, y, width, height)
+        self.texture = texture
+
+    def get_hover_info(self) -> str:
+        return f"{t(self.name)}: {t('barracks_hover_info')}"
+
+    def get_actions(self) -> list:
+        return [
+            {"label": f"{t('create_unit')}", "action": "create_unit"}
+        ]
